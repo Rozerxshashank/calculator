31d0c848b292f582c167243cbed11ba38c40d7cc
Rozer <yshashank959@gmail.com>
2025-07-24 09:11:04 +0530

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 const buttons=document.querySelectorAll(".button");
 const input=document.getElementById("input");
-const operators=document.querySelectorAll(".operator");
+const opp=['+','-','/','*','%'];
+function Result(){
+    try{
+        const exp=input.value
+        const result= new Function('return '+exp)();
+        addhistory(exp,result);
+        input.value=result;
+        
+    }   catch{
+            input.value="Error";
+        }
+} 
+function addhistory(exp,result){
+    const li=document.getElementById("history-list");
+    const list=document.createElement("li");
+    list.textContent=`${exp}=${result}`;
+    li.prepend(list);
+}
+function Appendvalue(value){
+    const lastchar=input.value.slice(-1);
+    if(input.value=="" && ['+','/','*','%'].includes(value))return;
+    if(opp.includes(value)&& opp.includes(lastchar))return;
+    input.value+=value;
+}
 buttons.forEach(button=>{
-    button.addEventListener("click",function(e){
-       if (button.innerHTML=="AC"){
+    button.addEventListener("click",function(){
+       const value=button.innerHTML;
+       if (value=="AC"){
         input.value="";
        }
-       else if(button.innerHTML=="Del"){
+       else if(value=="Del"){
         input.value = input.value.slice(0, -1);
        }
-       else if(button.innerHTML=="="){
-        input.value=eval(input.value);
-       }
+       else if(value=="="){
+        Result();}
        else{
-        input.value+=button.innerHTML;
+        Appendvalue(value);
        }
     })
     
